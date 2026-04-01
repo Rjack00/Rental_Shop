be3564b7effe12878bdc343017edcce17bebc711
Rjack00 <Rjackson77@comcast.net>
2026-03-31 19:06:27 -0600

    .sh updated

--- bike-shop.sh
+++ bike-shop.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+echo -e "\n~~ Bike Rental Shop ~~\n";
+
+MAIN_MENU () {
+  echo "How may I help you?"
+  echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
+  read MAIN_MENU_SELECTION
+}
+
+RENT_MENU () {
+  echo -e "Rent Menu"
+}
+
+RETURN_MENU () {
+  echo "Return Menu"
+}
+
+EXIT () {
+  echo -e "\nThank you for shopping in.\n"
+}
+
+MAIN_MENU
