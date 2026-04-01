07db7d7415da5931d5b6b466e33d92ac0945f7b9
Rjack00 <Rjackson77@comcast.net>
2026-03-31 18:35:08 -0600

    .sh added, .sql files updated

---
+++ data.sql
@@ -0,0 +1,6 @@
+INSERT INTO bikes(type, size) VALUES('Mountain', 27);
+INSERT INTO bikes(type, size) VALUES('Mountain', 28);
+INSERT INTO bikes(type, size) VALUES('Mountain', 29);
+INSERT INTO bikes(type, size) VALUES('Road', 27);
+INSERT INTO bikes(type, size) VALUES('Road', 28), ('Road', 29);
+INSERT INTO bikes(type, size) VALUES('BMX', 19), ('BMX', 20), ('BMX', 21);
